04740bd4454494b501d8ad6158129aa831463b18
nemoto <samuko.atuatu@gmail.com>
2020-04-09 05:44:14 +0000

    20.4.9-1 commit

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -4,21 +4,36 @@ namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Profile;
 
 class ProfileController extends Controller
 {
-    //
-    public function add(){
-        return view ('admin.profile.create');
+     public function add()
+    {
+        return view('admin.profile.create');
     }
-    public function create(){
-        return redirect ('admin/profile/create');
+
+    public function edit()
+    {
+        return view('admin.profile.edit');
     }
-    public function edit(){
-        return view ('admin.profile.edit');
-    }
-    public function update(){
-        return redirect ('admin/profile/edit');
+
+    public function update()
+    {
+        return redirect('admin/profile/edit');
     }
     
+     public function create(Request $request){
+        $this->validate($request, Profile::$rules);
+        $news = new Profile;
+        $form = $request->all();
+        // フォームから送信されてきた_tokenを削除する
+        unset($form['_token']);
+        // フォームから送信されてきたimageを削除する
+        // データベースに保存する
+        $news->fill($form);
+        $news->save();
+        return redirect('admin/profile/create');
+        
+    }
 }
--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -9,7 +9,7 @@
         <div class="row">
             <div class="col-md-8 mx-auto">
                 <h2>{{ __('messages.My Profile') }}</h2>
-                <form action="{{ action('Admin\NewsController@create') }}" method="post" enctype="multipart/form-data">
+                <form action="{{ action('Admin\ProfileController@create') }}" method="post" enctype="multipart/form-data">
 
                     @if (count($errors) > 0)
                         <ul>
@@ -21,29 +21,29 @@
                     <div class="form-group row">
                         <label class="col-md-2">{{ __('messages.name') }}</label>
                         <div class="col-md-10">
-                            <input type="text" class="form-control" name="title" value="{{ old('title') }}">
+                            <input type="text" class="form-control" name="name" value="{{ old('name') }}">
                         </div>
                     </div>
                     <div class="form-group row">
                         <label class="col-md-2">{{ __('messages.gender') }}</label>
                         <div class="col-md-10">
-                            <input type="text" class="form-control" name="title" value="{{ old('title') }}">
+                            <input type="text" class="form-control" name="gender" value="{{ old('gender') }}">
                         </div>
                     </div>
                     <div class="form-group row">
                         <label class="col-md-2">{{ __('messages.hobby') }}</label>
                         <div class="col-md-10">
-                            <textarea class="form-control" name="body" rows="10">{{ old('body') }}</textarea>
+                            <textarea class="form-control" name="hobby" rows="10">{{ old('hobby') }}</textarea>
                         </div>
                     </div>
                     <div class="form-group row">
                         <label class="col-md-2">{{ __('messages.introduction') }}</label>
                         <div class="col-md-10">
-                            <textarea class="form-control" name="body" rows="10">{{ old('body') }}</textarea>
+                            <textarea class="form-control" name="introduction" rows="10">{{ old('introduction') }}</textarea>
                         </div>
                     </div>
                     {{ csrf_field() }}
-                    <input type="submit" class="btn btn-primary" value="送信"　form action ='Admin\ProfileController@create'>
+                    <input type="submit" class="btn btn-primary" value="送信"　name = create>
                 </form>
             </div>
         </div>
